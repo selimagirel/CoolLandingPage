cb1203ea76e1f3aeedd0cf6cd973e98b71584e2b
selim <selimb768@gmail.com>
2024-12-02 18:55:35 +0300

    first

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,29 @@
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
+
+const footerLinks = [
+    { href: "#", label: "Contact" },
+    { href: "#", label: "Privacy Policy" },
+    { href: "#", label: "Terms & Conditions" },
+];
+
+export default function Footer() {
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row md:justify-between items-center gap-6">
+                    <div>
+                        <Image src={logoImage} alt="logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a href={link.href} className="text-white/50 text-sm" key={link.label}>{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
